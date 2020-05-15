de54b3a63d41ba7afb63e16a0f37c9ac50e8fe23
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-05-14 18:08:05 -0700

    Changed the seed data to show that the getRecipesByIngredient function works.

--- data/seeds/004-recipe_ingredient.js
+++ data/seeds/004-recipe_ingredient.js
@@ -8,6 +8,7 @@ exports.seed = function(knex) {
         { recipe_id: 1, ingredient_id: 1, quantity: 1, unit: "cup" },
         { recipe_id: 1, ingredient_id: 2, quantity: 1, unit: "cup" },
         { recipe_id: 2, ingredient_id: 3, quantity: 1 },
+        { recipe_id: 2, ingredient_id: 4, quantity: 1, unit: "slice" },
         { recipe_id: 3, ingredient_id: 4, quantity: 2, unit: "slices" },
         { recipe_id: 3, ingredient_id: 5, quantity: 1, unit: "slice" },
         { recipe_id: 3, ingredient_id: 6, quantity: 1, unit: "slice" },
